e4c830a0c53b367c6b26a44a3f3846882620114c
UnityBets <87617904+UnityBets@users.noreply.github.com>
2022-06-23 12:24:31 +0300

    Delete ShowDates.py

--- ShowDates.py
+++
@@ -1,4 +0,0 @@
-import requests
-response = requests.get('https://raw.githubusercontent.com/UnityBets/Database/main/members/python/members-python.data')
-#print (response.status_code)
-print (response.content)
